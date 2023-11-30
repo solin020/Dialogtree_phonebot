627846b2ff63f66ebd47cefde64c115f717866b5
Jacob Solinsky <solin020@umn.edu>
2023-11-30 15:13:58 -0600

    Restructure 2

--- start.sh
+++
@@ -1,4 +0,0 @@
-uvicorn --host 0.0.0.0 --port 80 app:app &
-uvicorn --host 127.0.0.1 --port 5001 tts:app &
-uvicorn --host 127.0.0.1 --port 5002 stt:app &
-uvicorn --host 127.0.0.1 --port 5003 llm:app &
